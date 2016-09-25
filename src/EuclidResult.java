8408a9355af3663add31d1e6a087df0fde1a64c2
Moritz <cmhoequist@gmail.com>
2016-09-25 08:50:22 -0500

    Added Conclusions.txt output

--- src/EuclidResult.java
+++ src/EuclidResult.java
@@ -1,20 +1,22 @@
 /**
- * Created by Moritz on 9/22/2016.
+ * Created by Bailey-Fawcett-Hoequist on 9/22/2016.
  *
- * Superclass for Euclid's algorithms.
+ * Superclass for Euclid's algorithms. Computes GCD and runtime when initialized.
  */
 public abstract class EuclidResult {
+    //Public for convenience
     public long numberOne;
     public long numberTwo;
     public long gcd;
-    public Long runtimeNS;
+    public Long runtimeNS;      //Long object permits use of compareTo(Long) method. Useful when sorting later.
     public double runtimeMS;
 
     protected void init(long num1, long num2){
-        long startTime;                         //used for timing the algorithms
+        //Initialize
+        long startTime;
         numberOne = num1; numberTwo = num2;
 
-        //calculate the GCD using Euclid's algorithm 1, and get the running time while we're at it
+        //Calculate the GCD and get the algorithm runtime
         startTime = System.nanoTime();
         gcd = runAlgorithm();
         runtimeNS = System.nanoTime() - startTime;
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.*;
 
 /**
- * Author: Bailey-Fawcett-Hoequist
+ * Created by Bailey-Fawcett-Hoequist on 9/22/2016
  */
 public class Main {
     private static int numPairs = 100;
@@ -14,15 +14,21 @@ public class Main {
     public static void main(String[] args) {
         EuclidOriginal[] originalRuns = new EuclidOriginal[numPairs];
         EuclidModified[] modifiedRuns = new EuclidModified[numPairs];
+        int improvedCount = 0;
+        double originalAverage, modifiedAverage;
 
-        //GENERATE 100 pairs of random integers
+        //Compute GCD and runtime for 100 pairs of random integers
         Random rand = new Random();
         for(int i = 0; i < numPairs; i++){
             long num1 = rand.nextInt(Integer.MAX_VALUE) + 1, num2 = rand.nextInt(Integer.MAX_VALUE) + 1;
             originalRuns[i] = new EuclidOriginal(num1, num2);
             modifiedRuns[i] = new EuclidModified(num1, num2);
+            //If modified algorithm represents a runtime improvement, record
+            if(originalRuns[i].runtimeNS > modifiedRuns[i].runtimeNS){
+                improvedCount += 1;
+            }
         }
-
+        //Sorting allows easy access to min, max, and median
         Arrays.sort(originalRuns, (EuclidOriginal a, EuclidOriginal b) -> a.runtimeNS.compareTo(b.runtimeNS));
         Arrays.sort(modifiedRuns, (EuclidModified a, EuclidModified b) -> a.runtimeNS.compareTo(b.runtimeNS));
 
@@ -34,11 +40,57 @@ public class Main {
 
         //Writing
         writeResults("Original_Euclid_Results.csv", originalRuns);
-        writeTable("Original_Euclid_Statistics.csv", originalRuns);
+        originalAverage = writeTable("Original_Euclid_Statistics.csv", originalRuns);
         writeResults("Improved_Euclid_Results.csv", modifiedRuns);
-        writeTable("Improved_Euclid_Statistics.csv", modifiedRuns);
+        modifiedAverage = writeTable("Improved_Euclid_Statistics.csv", modifiedRuns);
+        try(FileWriter writer = new FileWriter(new File("Conclusions.txt"))){
+            writer.write("Out of 100 pairs of integers, the improved(?) Euclid's algorithm outperformed the original " +
+                    "one in " + improvedCount + " pairs; and the average " +
+                    (modifiedAverage < originalAverage ? "saved" : "wasted") + " time for one pair of integers was " +
+                    (Math.abs(modifiedAverage - originalAverage)) + " milliseconds.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+    private static void writeResults(String filename, EuclidResult[] results){
+        try(FileWriter writer = new FileWriter(new File(filename))){
+            writer.write(resultsHeader + "\n");
+            for(int i = 0; i < numPairs; i++){
+                EuclidResult e = results[i];
+                writer.write(e.numberOne+","+e.numberTwo+","+e.gcd+","+e.runtimeMS+"\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Returns the average runtime in ms. Useful when composing Conclusions.txt.
+    private static double writeTable(String filename, EuclidResult[] results){
+        long max, min, median;
+        double average;                     //Computed average may not be an integer
+        min = results[0].runtimeNS;
+        max = results[numPairs -1].runtimeNS;
+        long sum = 0;
+        for(EuclidResult e : results){
+            sum += e.runtimeNS;
+        }
+        average = (double)sum / numPairs;
+        median = (results[49].runtimeNS + results[50].runtimeNS) / 2;
+
+        try(FileWriter writer = new FileWriter(new File(filename))){
+            writer.write(tableHeader + "\n");
+            writer.write("Minimum Time,"+min/1000000.0);
+            writer.write("Maximum Time,"+max/1000000.0);
+            writer.write("Average Time,"+average/1000000.0);
+            writer.write("Median Time,"+median/1000000.0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return average/1000000.0;
+    }
+
+    //DEBUGGING--------------------------------------------------------------------------------------------
     private static void printResults(EuclidResult[] results){
         System.out.format("%-10s\t%-10s\t%-10s\t%-10s\n","Number One","Number Two","GCD","Runtime (ms)");
         for(EuclidResult e : results){
@@ -58,40 +110,6 @@ public class Main {
         median = (results[49].runtimeNS + results[50].runtimeNS) / 2;
         System.out.format("%-10s\t%-10s\n","Statistic","Milliseconds");
         System.out.format("%-10s\t%-10f\n%-10s\t%-10f\n%-10s\t%-10f\n%-10s\t%-10f\n","Min",min/1000000.0,"Max",max/1000000.0,"Average",average/1000000.0,"Median",median/1000000.0);
-    }
-
-    private static void writeResults(String filename, EuclidResult[] results){
-        try(FileWriter writer = new FileWriter(new File(filename))){
-            writer.write(resultsHeader + "\n");
-            for(int i = 0; i < numPairs; i++){
-                EuclidResult e = results[i];
-                writer.write(e.numberOne+","+e.numberTwo+","+e.gcd+","+e.runtimeMS+"\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void writeTable(String filename, EuclidResult[] results){
-        long max, min, average, median;
-        min = results[0].runtimeNS;
-        max = results[numPairs -1].runtimeNS;
-        long sum = 0;
-        for(EuclidResult e : results){
-            sum += e.runtimeNS;
-        }
-        average = sum / numPairs;
-        median = (results[49].runtimeNS + results[50].runtimeNS) / 2;
-
-        try(FileWriter writer = new FileWriter(new File(filename))){
-            writer.write(tableHeader + "\n");
-            writer.write("Minimum Time,"+min/1000000.0);
-            writer.write("Maximum Time,"+max/1000000.0);
-            writer.write("Average Time,"+average/1000000.0);
-            writer.write("Median Time,"+median/1000000.0);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
 
